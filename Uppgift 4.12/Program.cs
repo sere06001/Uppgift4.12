0cd9c84f37773716e12886ab59acf6ade916749c
sere06001 <sere06001@utb.vaxjo.se>
2023-10-19 20:04:13 +0200

    Add project files.

---
+++ Uppgift 4.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm kvadraternas sidlängd");
+            int kvadratSidlängd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bestäm rutnätets bredd");
+            int rutnätBredd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bestäm rutnätets höjd");
+            int rutnätHöjd = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (int antalKvadrater = 0; antalKvadrater < rutnätHöjd; antalKvadrater++)
+            {
+                for (int i = 0; i < kvadratSidlängd; i++)
+                {
+                    string FemStjärnor = "***** ";
+                    string utskrivet = string.Concat(Enumerable.Repeat(FemStjärnor, rutnätBredd));
+                    Console.WriteLine($"{utskrivet}");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
